ea46cae6bd7153a0c1a601fac8676fd69b18fec4
LACHHAB AYOUB <44031876+LACHHABAYOUB@users.noreply.github.com>
2021-11-18 10:39:39 -0600

    Add files via upload

--- isSelfReferential.java
+++ isSelfReferential.java
@@ -16,7 +16,7 @@ public class Test {
         
         for (int i = 0; i < a.length; i++) {
             int count = 0;
-            for (int j = 0; j < a.length; j++) {
+            for (int j= 0; j< a.length; j++) {
                 if (a[j] == i)
                     count++;
             }
